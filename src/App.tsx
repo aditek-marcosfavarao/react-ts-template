9a69ca9b85ace3df524549d4675d6a56ddf59a7d
marcosfavarao <marcos.favarao@aditek.com.br>
2025-05-28 15:23:36 -0300

    feat: global style/theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '@common/styles/global';
+import { defaultTheme } from '@common/styles/themes/default';
+
 export function App() {
 	return (
 		<>
-			<h1>hello world!</h1>
+			<ThemeProvider theme={defaultTheme}>
+				<h1>hello world!</h1>
+
+				<GlobalStyle />
+			</ThemeProvider>
 		</>
 	);
 }
