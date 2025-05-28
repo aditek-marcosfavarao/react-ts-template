e8fe62b521b1f82b3ef615690b996aadf11b3fa8
marcosfavarao <marcos.favarao@aditek.com.br>
2025-05-28 15:27:02 -0300

    feat: base blank component

---
+++ src/components/Blank/Blank.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	width: 100%;
+	height: 100svh;
+	padding: 0.5rem;
+	margin: 0 auto;
+`;
---
+++ src/components/Blank/Blank.tsx
@@ -0,0 +1,11 @@
+import { Wrapper } from './Blank.styles';
+
+export function Blank() {
+	return (
+		<>
+			<Wrapper>
+				<h1>blank component</h1>
+			</Wrapper>
+		</>
+	);
+}
